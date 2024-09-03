773785490807575166542a7a6973d2602bdf8e45
Aman Anurag <aanurag@sensation.com>
2024-09-03 10:37:41 +0530

    event listener

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const About = () => {
+  const [count, setCount] = useState(100);
 
-    const [count, setCount]= useState(100);
-    // console.log("hiiiiiiiii");
-  
-    useEffect(()=>{
-        const id=setInterval(()=>{
-            console.log("set Interval called")
-        },2000);
+  useEffect(() => {
+    const id=setInterval(() => {
+      console.log("set Interval called");
+    }, 2000);
 
-        return ()=>{
-            console.log("return useEffect");
-            clearInterval(id);
-            //clean Up logic
-        }
-    },[])
-   
+    return ()=>{
+        console.log("return useEffect")
+        clearInterval(id)
+    }
+  }, []);
+
   return (
     <div>
-        About
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIApD8eXIeXXuTpT3tJbqdNOJtTGMVMiRLYg&s' alt='img'/>
-
-        <button onClick={()=>{setCount(count+100)}}>Increase count</button>
-        {count}
+      About
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIApD8eXIeXXuTpT3tJbqdNOJtTGMVMiRLYg&s"
+        alt="img"
+      />
+      <button
+        onClick={() => {
+          setCount(count + 100);
+        }}
+      >
+        Increase count
+      </button>
+      {count}
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Login = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMove=(event) => {
+      console.log("fxn executes");
+      const newposition = { x: event.clientX, y: event.clientY };
+      setMousePosition(newposition);
+    };
+
+    window.addEventListener("mousemove",handleMove) 
+
+    return () => {
+      window.removeEventListener("mousemove", handleMove);
+    };
+  }, []);
+
   return (
-    <div>Login
+    <div className="flex">
+      <img
+        src="https://img.uxcel.com/tags/login-1698251086462-2x.jpg"
+        alt="img"
+        className="w-[700px]"
+      />
 
-        <img src='https://img.uxcel.com/tags/login-1698251086462-2x.jpg' alt='img' className='w-[700px]'/>
+      <div className="text-4xl text-orange-600">
+        {mousePosition.x}/{mousePosition.y}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
